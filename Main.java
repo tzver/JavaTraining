99a9cedd96cdda785f441ee0dbfa64c563368279
unknown <tara.zver22@gmail.com>
2022-10-15 07:51:56 +0200

    New

--- Main.java
+++ Main.java
@@ -4,7 +4,14 @@ package JavaTraining;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+
+    public static void main(String args[]) {
+        // main(122);
+        // main(1.2);
+        main("hello java");
+    }
+
+    public static void main(int i) {
         double gpa = 4.0;
         String firstName = "Tara";
         String lastName = "Zver";
@@ -15,7 +22,46 @@ public class Main {
         Scanner input = new Scanner(System.in);
         double GPA = input.nextDouble();
         gpa = GPA;
-        System.out.println(firstName + " " + lastName + " has a GPA of " + gpa + ".");
+        if (gpa < 1.0) {
+            System.out.println("You have failed the exam. You should study more and retake it.");
+        } else {
+            System.out.println("Congratulations! You have passed the exam.");
+        }
 
     }
+
+    public static void main(double i) {
+        Scanner input = new Scanner(System.in);
+        boolean isOnRepeat = true;
+        while (isOnRepeat) {
+            System.out.println("Playing song");
+            System.out.println("Take off repeat? Respond with y or n.");
+            String userInput = input.next();
+            if (userInput.equals("y")) {
+                isOnRepeat = false;
+            }
+
+        }
+    }
+
+    public static void main(String j) {
+        String question = "How are you?";
+        String choiceOne = "bad";
+        String choiceTwo = "good";
+        String choiceThree = "so so";
+
+        String correct = choiceTwo;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println(question);
+        String answer = input.next();
+
+        if (correct.equals(answer)) {
+            System.out.println("You are correct!");
+        } else {
+            System.out.println("You are incorrect :(");
+
+        }
+    }
+
 }
