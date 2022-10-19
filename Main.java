22b8db237da2bc396263f86947bd4cdbdc9a3c46
unknown <tara.zver22@gmail.com>
2022-10-19 17:59:35 +0200

    New

---
+++ Main.java
@@ -0,0 +1,21 @@
+
+package JavaTraining;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        double gpa = 4.0;
+        String firstName = "Tara";
+        String lastName = "Zver";
+        System.out.println(firstName + " " + lastName);
+
+        System.out.println(firstName.charAt(0) + " " + lastName.charAt(0));
+
+        Scanner input = new Scanner(System.in);
+        double GPA = input.nextDouble();
+        gpa = GPA;
+        System.out.println(firstName + " " + lastName + " has a GPA of " + gpa);
+
+    }
+}
