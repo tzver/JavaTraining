daaf62c863fdf7d5bfa6aadce7bda0e341911585
unknown <tara.zver22@gmail.com>
2022-10-21 08:25:19 +0200

    New changes section 5.

--- Main.java
+++ Main.java
@@ -4,18 +4,40 @@ package JavaTraining;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        double gpa = 4.0;
-        String firstName = "Tara";
-        String lastName = "Zver";
-        System.out.println(firstName + " " + lastName);
 
-        System.out.println(firstName.charAt(0) + " " + lastName.charAt(0));
-
+    public static void announceDeveloperTeaTime() {
+        System.out.println("Waiting for developer tea time ...");
+        System.out.println("Type in a random word and press Enter to start developer tea time");
         Scanner input = new Scanner(System.in);
-        double GPA = input.nextDouble();
-        gpa = GPA;
-        System.out.println(firstName + " " + lastName + " has a GPA of " + gpa + ".");
+        input.next();
+        System.out.println("It's developer tea time!");
 
     }
+
+    public static double totalMealCost(double mealPrice, double tipRate, double taxRate) {
+        double tip = tipRate * mealPrice;
+        double tax = taxRate * mealPrice;
+        double result = mealPrice + tip + tax;
+        System.out.println("Your total meal price is " + result);
+        return result;
+    }
+
+    public static double calculateSalary(int numberOfHoursPerWeek, double hourlyWage, int vacationDays) {
+        int nrWeeksInYear = 52;
+        double yearlyWageWithoutHoliday = nrWeeksInYear * numberOfHoursPerWeek * hourlyWage;
+        double yearlyWageWithHoliday = yearlyWageWithoutHoliday - vacationDays * 8 * hourlyWage;
+
+        return yearlyWageWithHoliday;
+
+    }
+
+    public static void main(String args[]) {
+        // announceDeveloperTeaTime();
+        // double groupTotalMealCost = totalMealCost(25, 2, 0.08);
+        // System.out.println("Every person owes " + groupTotalMealCost / 3 + " euro.");
+        double yearlyWage = calculateSalary(40, 20, 23);
+        System.out.println("The employee makes " + yearlyWage + " euro per year.");
+
+    }
+
 }
